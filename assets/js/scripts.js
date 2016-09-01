fd70f98150d3559ec8691e5c8764beb463d8c5a1
Kennet Postigo <kennetfpostigo@gmail.com>
2016-09-01 15:23:43 -0400

    acm name generator

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
-
+	var acm = [
+		'American Cheese Monkeys',
+		'Amazingly Correct Maps',
+	  'Awkward Corrupt Manatees',
+		'Alien Chocolate Marbles',
+		'Association for Calculating Men',
+		'All Cheese Museum',
+		'Animals Closing Markets',
+		'Actual Cool Manerisms',
+		'Apparent Chapstick Messes',
+		'And Cheese Mac',
+		'Angelo Can’t Meme',
+		'Annabelle Crayon Monument',
+		'Asians Crazy for Mark',
+		'Air Creates Magic',
+		'Actual Code is a Myth',
+		'Amazing Colorful Megaphones',
+		'Angelos Creates Megaphones',
+		'Act Create Move',
+		'Alive Correct Me',
+		'Angel’s Copper Memoriam',
+		'Alive Chocolate Mannequins',
+		'Amazing Coding Memes',
+	];
+	$('.cuuuuuuuuuuuuuuuuuuuuuuuuuhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh').text(acm[Math.floor(Math.random()*acm.length)]);
 	$('ul.tabs li').click(function(){
 		var tab_id = $(this).attr('data-tab');
 
